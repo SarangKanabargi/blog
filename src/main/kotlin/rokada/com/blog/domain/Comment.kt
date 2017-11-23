81cea18a995df650d776d3cb3afbf659e4af0539
SarangKanabargi <sarang.kanabargi@gmail.com>
2017-11-22 18:48:42 -0700

    Initial Commit

--- src/main/kotlin/rokada/com/blog/domain/Comment.kt
+++
@@ -1,8 +0,0 @@
-package rokada.com.blog.domain
-
-import java.time.LocalDateTime
-
-data class Comment(val id: String?,
-                   val text: String,
-                   val commentBy: String,
-                   val commentDate: LocalDateTime = LocalDateTime.now())
